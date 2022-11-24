f57b266999fb1878d1d0c90ffcc0cba2bd9b6ba5
siduki <giorgigagnidze@gmail.com>
2022-11-24 14:10:35 +0400

    version 0.2, add input, no auto refresh

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,4 @@
-const userChoose = prompt("Enter your choose ");
+let userChoose = document.getElementById("userinput").value;
 function rockPapperScissor() {
   const random = ["Rock", "Papper", "Scissor"];
   document.getElementById("userchoose").innerHTML = userChoose;
@@ -9,46 +9,55 @@ function rockPapperScissor() {
     cpuChoose === "Rock"
   ) {
     document.getElementById("result").innerHTML = "Draw";
+    document.getElementById("result").style.color = "brown";
   } else if (
     (userChoose === "Rock" || userChoose === "rock") &&
     cpuChoose === "Papper"
   ) {
     document.getElementById("result").innerHTML = "You loose";
+    document.getElementById("result").style.color = "red";
   } else if (
     (userChoose === "Rock" || userChoose === "rock") &&
     cpuChoose === "Scissor"
   ) {
     document.getElementById("result").innerHTML = "You win";
+    document.getElementById("result").style.color = "green";
   } else if (
     (userChoose === "Papper" || userChoose === "papper") &&
     cpuChoose === "Rock"
   ) {
     document.getElementById("result").innerHTML = "You win";
+    document.getElementById("result").style.color = "green";
   } else if (
     (userChoose === "Papper" || userChoose === "papper") &&
     cpuChoose === "Papper"
   ) {
     document.getElementById("result").innerHTML = "Draw";
+    document.getElementById("result").style.color = "black";
   } else if (
     (userChoose === "Papper" || userChoose === "papper") &&
-    cpuChoose === "Rock"
+    cpuChoose === "Scissor"
   ) {
     document.getElementById("result").innerHTML = "You loose";
+    document.getElementById("result").style.color = "red";
   } else if (
     (userChoose === "Scissor" || userChoose === "scissor") &&
     cpuChoose === "Rock"
   ) {
     document.getElementById("result").innerHTML = "You loose";
+    document.getElementById("result").style.color = "red";
   } else if (
     (userChoose === "Scissor" || userChoose === "scissor") &&
     cpuChoose === "Scissor"
   ) {
     document.getElementById("result").innerHTML = "Draw";
+    document.getElementById("result").style.color = "black";
   } else if (
     (userChoose === "Scissor" || userChoose === "scissor") &&
     cpuChoose === "Papper"
   ) {
     document.getElementById("result").innerHTML = "You win";
+    document.getElementById("result").style.color = "green";
   }
 }
 
